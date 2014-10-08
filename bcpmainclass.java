711715a81104798b2a7682ead5fa0d482797e8e5
ibornlp <lp.iborn@gmail.com>
2014-10-08 10:10:44 +0200

    Create bcpmainclass.java

---
+++ bcpmainclass.java
@@ -0,0 +1,43 @@
+	
+
+    package bcp;
+     
+    import org.bukkit.GameMode;
+    import org.bukkit.command.Command;
+    import org.bukkit.command.CommandSender;
+    import org.bukkit.entity.Player;
+    import org.bukkit.plugin.java.JavaPlugin;
+    //Server Boot Message
+    public class bcpmain extends JavaPlugin {
+            public void onEnable()  {
+                    System.out.println("[bcp] geladen");
+            }
+            public void onDisable()  {
+                    System.out.println("[bcp] gestoppt");
+            }
+            //gm0
+                    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+                    Player p = (Player)sender;
+                    if(cmd.getName().equalsIgnoreCase("gm0"))
+                            if(p.hasPermission("bcp.gm0"))
+                            p.setGameMode(GameMode.SURVIVAL);
+                           
+                    //gm1  
+                   
+                    if(cmd.getName().equalsIgnoreCase("gm1"))
+                            if(p.hasPermission("bcp.gm1"))
+                                    p.setGameMode(GameMode.CREATIVE);
+           
+                   
+                    //gm2
+                    if(cmd.getName().equalsIgnoreCase("gm2"))
+                            if(p.hasPermission("bcp.gm2"))
+                                    p.setGameMode(GameMode.ADVENTURE);
+                   
+                    //h
+                    if(cmd.getName().equalsIgnoreCase("h"))
+                            if(p.hasPermission("bcp.heal"))
+                                    p.setHealthScale(20);
+                   
+                    return true;
+
