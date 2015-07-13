3ab4ea2e6844e565d639a3c97a2d05493dec9def
FroznMine <pesafrre@arcor.de>
2015-07-13 17:05:00 +0200

    Output bug gefixt
    Fixed #13

--- src/bcp/BCP.java
+++ src/bcp/BCP.java
@@ -44,56 +44,53 @@ public class BCP extends JavaPlugin {
 		Player p = (Player) sender;
 
 		//gm0
-		if(cmd.getName().equalsIgnoreCase("gm0")) {
-			if(p.hasPermission("bcp.gm0"))
-				p.setGameMode(GameMode.SURVIVAL);
+		if (cmd.getName().equalsIgnoreCase("gm0") && p.hasPermission("bcp.gm0")) {
+			p.setGameMode(GameMode.SURVIVAL);
 			p.sendMessage(this.getConfig().getString("bcp.commands.gm0.messages.Gamemode Changed to Survival"));
 		}
 
 		//gm1
-		if(cmd.getName().equalsIgnoreCase("gm1")) {
-			if(p.hasPermission("bcp.gm1"))
-				p.setGameMode(GameMode.CREATIVE);
+		if (cmd.getName().equalsIgnoreCase("gm1") && p.hasPermission("bcp.gm1")) {
+			p.setGameMode(GameMode.CREATIVE);
 			p.sendMessage(this.getConfig().getString("bcp.commands.gm1.messages.Gamemode Changed to Creative"));
 		}
 
 		//gm2
-		if(cmd.getName().equalsIgnoreCase("gm2")) {
-			if(p.hasPermission("bcp.gm2"))
-				p.setGameMode(GameMode.ADVENTURE);
+		if (cmd.getName().equalsIgnoreCase("gm2") && p.hasPermission("bcp.gm2")) {
+			p.setGameMode(GameMode.ADVENTURE);
 			p.sendMessage(this.getConfig().getString("bcp.commands.gm2.messages.Gamemode Changed to Adventure"));
 		}
 
 		//h
-		if(cmd.getName().equalsIgnoreCase("h"))
-			if(p.hasPermission("bcp.heal"))
-				p.setHealth(20F);
-		p.sendMessage("bcp.commands.h.healed");
+		if (cmd.getName().equalsIgnoreCase("h") && p.hasPermission("bcp.heal")) {
+			p.setHealth(20F);
+			p.sendMessage(this.getConfig().getString("bcp.commands.h.healed"));
+		}
 
 		//xp
-		if(cmd.getName().equalsIgnoreCase("xpf"))
-			if(p.hasPermission("bcp.xp"))
-				p.setExp(p.getExp() + 1000);
-		p.sendMessage("bcp.commands.xpf.XP Filled");
+		if (cmd.getName().equalsIgnoreCase("xpf") && p.hasPermission("bcp.xp")) {
+			p.setExp(p.getExp() + 1000);
+			p.sendMessage(this.getConfig().getString("bcp.commands.xpf.XP Filled"));
+		}
 
 		//food
-		if(cmd.getName().equalsIgnoreCase("food"))
-			if(p.hasPermission("bcp.food"))
-				p.setFoodLevel(20);
-		p.sendMessage("bcp.commands.food.Food Filled !");
+		if (cmd.getName().equalsIgnoreCase("food") && p.hasPermission("bcp.food")) {
+			p.setFoodLevel(20);
+			p.sendMessage(this.getConfig().getString("bcp.commands.food.Food Filled !"));
+		}
 
 		//Biome
-		if(cmd.getName().equalsIgnoreCase("bio"))
-			if (p.hasPermission("bcp.bio"))
-				p.sendMessage(p.getWorld().getBiome(p.getLocation().getBlockX(), p.getLocation().getBlockZ()).name());
+		if (cmd.getName().equalsIgnoreCase("bio") && p.hasPermission("bcp.bio"))
+			p.sendMessage(p.getWorld().getBiome(p.getLocation().getBlockX(), p.getLocation().getBlockZ()).name());
 
 		//BCP
-		if(cmd.getName().equalsIgnoreCase("bcp"))
+		if (cmd.getName().equalsIgnoreCase("bcp"))
 			p.sendMessage("BetterCommands works perfect !");
 
 		//BetterCommands
 		if(cmd.getName().equalsIgnoreCase("bettercommands"))
 			p.sendMessage("BetterCommands Plugin works perfect!");
+
 		return true;
 	}
 }
